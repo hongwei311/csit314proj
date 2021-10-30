bad2dcc54dd1957bb844d131436b51904143383d
hongwei311 <91199174+hongwei311@users.noreply.github.com>
2021-10-30 14:48:10 +0800

    add comments for admin add user

--- Admin_Add_User_Page.php
+++ Admin_Add_User_Page.php
@@ -1,6 +1,6 @@
 <?php
-include_once("UserController.php");
-session_start();
+include_once("UserController.php"); // include User Controller to call function
+session_start(); // start session to manipulate session variables
 ?>
 <!DOCTYPE html>
 <html>
@@ -55,23 +55,27 @@ table, th, td {
 <p><a href="Admin_Main_Page.php"><button class="button">Main Page</button></p></a></p>
 
 <form id="AdminAddUserPage" method="POST" action="Admin_Add_User_Page.php">
+  <!-- create form wih post method to the same page -->
   <label>Username: </label>
   <input type="text" id="Username" name="username"><br><br>
+  <!-- create input text for Username for user to input username text -->
   <label>Password: </label>
   <input type="Password" id="Password" name="password"><br><br>
+  <!-- create password text for Username for user to input username text -->
   <label>User Profile</label>&ensp;<select name = "userprofile" id="userprofile">
             <option value = "Admin" >Admin</option>
             <option value = "Doctor" >Doctor</option>
             <option value = "Patient" >Patient</option>
             <option value = "Pharmacist" >Pharmacist</option>
         </select> <br><br>
+   <!-- create option input for User Profile for user to select user profile -->
   <input type="submit" value="Submit">
 </form> 
 <?php
 if($_SERVER['REQUEST_METHOD']=='POST')
 {
-  $UserControl = new UserControl();
-  $validation = $UserControl->addUser($_POST['username'],$_POST['password'],$_POST['userprofile']);
+  $UserControl = new UserControl(); // create User Controller to run function
+  $validation = $UserControl->addUser($_POST['username'],$_POST['password'],$_POST['userprofile']); //assign output from addUser function to validation
   if($validation==true)
   {
     echo "User added successfully";
