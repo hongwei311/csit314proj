3af60d2854065602f03d71f0fc0c14e12dadf1d0
Dominic Loh <dominic.loh@hotmail.com>
2021-11-04 22:28:43 +0800

    Bootstrap

--- Admin_View_User_Info_Page.php
+++ Admin_View_User_Info_Page.php
@@ -9,6 +9,9 @@ session_start();
 <head>
 <title>View User Info</title>
 	<link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
 </head>
 <body>
 
